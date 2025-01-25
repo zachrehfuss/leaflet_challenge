2a8592a6750f0328e4f4d81bf39b0c3f309d21a4
zach <rehfusszach@gmail.com>
2025-01-25 13:24:16 -0600

    first chunk of code done, working through setting markers

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # leaflet_challenge
+
+Code notes
+- alice: 
+- Xpert learning assistant: helped debug code for color function as syntax was slightly off,
+syntax for stylefunction
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,6 @@
 // Create the 'basemap' tile layer that will be the background of our map.
-
+let map = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
 
 // OPTIONAL: Step 2
 // Create the 'street' tile layer as a second background of the map
@@ -22,34 +23,56 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
-
+    return {
+      color: getColor(depth),
+      fillColor: getColor(depth),
+      radius: getRadius(magnitude)
+    }
   }
 
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
-
-  }
+    for (let i = 0; i < depth.length; i++) {
+    let color = "";
+    if (depth[i].features.geometry.coordinates[2] <= 10) {
+      color = "green";}
+    else if (depth[i].features.geometry.coordinates[2] <= 30) {
+      color = "lime";}
+    else if (depth[i].features.geometry.coordinates[2] <= 50) {
+      color = "yellow";}
+    else if (depth[i].features.geometry.coordinates[2] <= 70) {
+      color = "orange";}
+    else if (depth[i].features.geometry.coordinates[2] <90) {
+      color = "red";}
+    else {color = "black";}
+    return color;
+  }}
 
   // This function determines the radius of the earthquake marker based on its magnitude.
   function getRadius(magnitude) {
-
+    return  Math.sqrt(magnitude) * 50;
   }
+
 
   // Add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
     // Turn each feature into a circleMarker on the map.
     pointToLayer: function (feature, latlng) {
-
-    },
+      return L.circleMarker([magnitude[i].features.geometry.coordinates[1],
+           magnitude[i].features.geometry.coordinates[0]], {
+             stroke: false,
+             fillOpacity: 0.75,
+             color: getColor(depth),
+             fillColor: getColor(depth),
+             radius: getRadius(magnitude[i].features.mag)
+    }},
     // Set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
 
     }
-  // OPTIONAL: Step 2
-  // Add the data to the earthquake layer instead of directly to the map.
-  }).addTo(map);
+}).addTo(map);
 
   // Create a legend control object.
   let legend = L.control({
